914a39cc59b6f07a5b4b253956b51978358105cb
saadsaifse <msaadsaif5@gmail.com>
2019-04-03 19:22:11 +0200

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,26 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<p align="center">
+  <h2 align="center">Markdown Previewer</h2>
+  <p align="center">A simple markdown previewer built using React<p>
+  <p align="center">
+    <a href="https://github.com/saadsaifse/markdown-previewer">
+      <img src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+    </a>
+    <a href="https://github.com/saadsaifse/markdown-previewer">
+    	<img src="https://img.shields.io/badge/platform-macOS%20%7C%20Windows%20%7C%20Linux-blue.svg" alt="platforms" />
+    </a>
+    <a href="https://github.com/saadsaifse/markdown-previewer">
+	    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg" alt="prs welcome">
+    </a>
+  </p>
+</p>
 
-## Available Scripts
+The project uses <a href="https://github.com/markedjs/marked">Marked parser</a> and React to preview user entered markdown in a user friendly way.
 
-In the project directory, you can run:
+**Usage:**
 
-### `npm start`
+Clone the repository, install the dependencies and start the app
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+```bash
+npm install
+npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./main.css";
+import marked from "marked";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const defaultMarkdown = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)`;
+
+    this.state = {
+      userInput: defaultMarkdown,
+      enlargedEditor: false,
+      enlargedPreview: false
+    };
+
+    marked.setOptions({ breaks: true });
+
+    this.handleInput = this.handleInput.bind(this);
+    this.resizeEditor = this.resizeEditor.bind(this);
+    this.resizePreviewer = this.resizePreviewer.bind(this);
+  }
+
+  resizeEditor() {
+    this.setState({
+      enlargedEditor: !this.state.enlargedEditor
+    });
+  }
+
+  resizePreviewer() {
+    this.setState({
+      enlargedPreview: !this.state.enlargedPreview
+    });
+  }
+
+  handleInput(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+
   render() {
+    const classes = this.state.enlargedEditor
+      ? ["maximized", "hide", "fa-compress-arrows-alt"]
+      : this.state.enlargedPreview
+      ? ["hide", "maximized", "fa-compress-arrows-alt"]
+      : ["", "", "fa-expand-arrows-alt"];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="container">
+        <div className={`editor ${classes[0]}`}>
+          <div className="toolbar">
+            <i className="fas fa-pen" />
+            <label>Editor</label>
+            <i
+              className={`fas ${classes[2]} fa-arrows-alt`}
+              onClick={this.resizeEditor}
+            />
+          </div>
+          <textarea id="editor" onInput={this.handleInput} value={this.state.userInput} />
+        </div>
+        <div id="previewer" className={`previewer ${classes[1]}`}>
+          <div className="toolbar">
+            <i className="fab fa-markdown" />
+            <label>Previewer</label>
+            <i
+              className={`fas ${classes[2]} fa-arrows-alt`}
+              onClick={this.resizePreviewer}
+            />
+          </div>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.userInput) }}
+          />
+        </div>
       </div>
     );
   }
